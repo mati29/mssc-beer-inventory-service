f5231d855844a56e3faa000ff0a349c0281a8cc4
Mati <mering20@gmail.com>
2020-11-24 18:04:49 +0100

    fullfiled allocation process (a lot of changes, review in future, which is working or not)

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationListener.java
@@ -0,0 +1,42 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.model.events.AllocateOrderRequest;
+import guru.sfg.brewery.model.events.AllocateOrderResult;
+import guru.sfg.brewery.model.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AllocationListener {
+
+    private final AllocationService allocationService;
+    private final JmsTemplate jmsTemplate;
+
+    @Transactional
+    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+    public void listener(AllocateOrderRequest request) {
+        AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
+        builder.beerOrderDto(request.getBeerOrderDto());
+
+        try {
+            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+
+            builder.pendingInventory(!allocationResult);
+        } catch (Exception e) {
+            log.error("Allocation failed for Order Id: {}", request.getBeerOrderDto().getId());
+            builder.allocationError(true);
+        }
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, builder.build());
+    }
+
+}
